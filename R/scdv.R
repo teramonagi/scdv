640890afb48f523259c83274d7dff6c951a43708
Nagi Teramo <teramonagi@gmail.com>
2019-02-11 18:58:49 +0900

    Yeeeeeeeeeeah

---
+++ R/scdv.R
@@ -0,0 +1,42 @@
+
+document_vector <- function(doc, wtv){
+
+}
+
+scdv <- function(){
+
+}
+
+word2vec <- function(doc, output_file = NULL){
+  document_file <- tempfile()
+  if(is.null(output_file)){
+    output_file <- tempfile(fileext = ".bin")
+  }
+  # Save doc into temp file
+  writeLines(stringr::str_c(unlist(doc), collapse = " "), document_file)
+  # Train a model by word2vec
+  model <- wordVectors::train_word2vec(document_file, output_file, force=TRUE)
+  as.matrix(model@.Data)
+}
+
+gmm <- function(word_vector, k){
+  x <- ClusterR::center_scale(word_vector, mean_center = T, sd_scale = T)
+  gmm <-  ClusterR::GMM(x, k, dist_mode = "maha_dist", seed_mode = "random_subset", km_iter = 10, em_iter = 10)
+  # predict centroids, covariance matrix and weights
+  ClusterR::predict_GMM(x, gmm$centroids, gmm$covariance_matrices, gmm$weights)$cluster_proba
+}
+
+calc_idf <- function(word, doc){
+  D <- length(doc)
+  denominator <- colSums(matrix(unlist(purrr::map(doc, ~ word %in% .x)), nrow = D, byrow = TRUE))
+  1 + log(D / denominator)
+}
+
+word_topics <- function(doc, w2v_args = list(use_), gmm_args){
+  wv <- word2vec(doc, output_file = NULL)
+  prob <- gmm(wv, k)
+  wcv <- purrr::map(seq_len(k), ~ prob[,.x]*wv)
+  # Calculate Inverse-Document-Frequency
+  idf <- calc_idf(dimnames(wv)[[1]], doc)
+  idf * purrr::reduce(wcv, ~ cbind(.x, .y))
+}
