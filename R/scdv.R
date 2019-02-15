037836a4505cb5899454d7422ab4386a8f4c6938
Nagi Teramo <teramonagi@gmail.com>
2019-02-15 09:57:08 +0900

    yeeeee

--- R/scdv.R
+++ R/scdv.R
@@ -1,51 +1,78 @@
+get_default <- function(func){
+  purrr::keep(x, purrr::map_lgl(as.list(formals(func)), ~ !is.name(.x)))
+}
 
-word2vec <- function(doc, output_file = NULL){
+overwrite_default <- function(func, args){
+  default_args <- get_default(func)
+  result <- purrr::map(names(default_args), ~ if(is.element(.x, args)){args[[.x]]}else{default_args[[.x]]})
+  setNames(result, names(default_args))
+}
+
+#' @export
+word2vec <- function(doc, dimension, args = list())
+{
+  output_file <- tempfile(fileext = ".bin")
   document_file <- tempfile()
-  if(is.null(output_file)){
-    output_file <- tempfile(fileext = ".bin")
-  }
   # Save doc into temp file
   writeLines(stringr::str_c(unlist(doc), collapse = " "), document_file)
   # Train a model by word2vec
-  model <- wordVectors::train_word2vec(document_file, output_file, force=TRUE)
+  model <- wordVectors::train_word2vec(document_file, output_file = output_file, vectors = dimension, force = TRUE)
   as.matrix(model@.Data)
 }
 
-gmm <- function(word_vector, k){
+#' @export
+gmm <- function(word_vector, k, args = list())
+{
   x <- ClusterR::center_scale(word_vector, mean_center = T, sd_scale = T)
   gmm <-  ClusterR::GMM(x, k, dist_mode = "maha_dist", seed_mode = "random_subset", km_iter = 10, em_iter = 10)
   # predict centroids, covariance matrix and weights
   ClusterR::predict_GMM(x, gmm$centroids, gmm$covariance_matrices, gmm$weights)$cluster_proba
 }
 
-calc_idf <- function(word, doc){
+#' @export
+calc_idf <- function(doc, word)
+{
   D <- length(doc)
   denominator <- colSums(matrix(unlist(purrr::map(doc, ~ word %in% .x)), nrow = D, byrow = TRUE))
   1 + log(D / denominator)
 }
 
-word_topics <- function(doc, w2v_args = list(use_), gmm_args){
-  wv <- word2vec(doc, output_file = NULL)
-  prob <- gmm(wv, k)
-  wcv <- purrr::map(seq_len(k), ~ prob[,.x]*wv)
-  idf <- calc_idf(dimnames(wv)[[1]], doc)
+#' @export
+word_topic_vector <- function(doc, k, dimension, word2vec_args = list(), gmm_args = list())
+{
+  wv <- word2vec(doc, dimension, word2vec_args)
+  prob <- gmm(wv, k, gmm_args)
+  wcv <- purrr::map(seq_len(ncol(prob)), ~ prob[,.x]*wv)
+  word <- rownames(wv)
+  idf <- calc_idf(word, doc)
   idf * purrr::reduce(wcv, ~ cbind(.x, .y))
 }
 
-make_sparse <- function(dv, p){
+make_sparse <- function(dv, p)
+{
   t <- mean(abs(range(dv)))
   ifelse(abs(dv) > p * t, dv, 0)
 }
 
+document_vector <- function(doc, word, wtv)
+{
+  purrr::map(doc, ~ colSums(wtv[.x[.x %in% word], ]))
+}
+
+
+#' Calculate Sparse Composite Document Vector (SCDV)
+#'
+#' Calculate Sparse Composite Document Vector (SCDV)
+#'
 #' @export
-#' @param doc list of document. each document
-scdv <- function(doc){
-  wv <- word2vec(doc)
-  prob <- gmm(wv, k)
-  wcv <- purrr::map(seq_len(k), ~ prob[,.x]*wv)
-  word <- rownames(wv)
-  idf <- calc_idf(word, doc)
-  wtv <- idf * purrr::reduce(wcv, ~ cbind(.x, .y))
-  dv <- purrr::map(doc, ~ colSums(wtv[.x[.x %in% word], ]))
-  purrr::map(dv, ~ make_sparse(.x))
+#' @param doc List of document. each document
+#' @param k The number of clusters
+#' @param dimension The dimensions of word vector representations for every word
+#' @param p The sparsity threshold for SCDV
+#' @param word2vec_args Parameters for wrod2vec model ( parameters of wordVectors::train_word2vec )
+#' @param gmm_args Parameters for GMM model ( parameters of ClusterR::center_scale and ClusterR::GMM )
+scdv <- function(doc, k, dimension, p = 0.01, word2vec_args = list(), gmm_args = list()){
+  wtv <- word_topic_vector(doc, k, dimension, word2vec_args, gmm_args)
+  dv <- document_vector(doc, word, wtv)
+  purrr::map(dv, ~ make_sparse(.x, p))
 }
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # scdv
 An R package for SCDV (Sparse Composite Document Vectors) algorithm
 
-#
+[![Travis-CI Build
+Status](https://api.travis-ci.com/teramonagi/scdv.svg?branch=master)](https://travis-ci.com/teramonagi/scdv)
+[![CRAN\_Status\_Badge](https://www.r-pkg.org/badges/version/gghighlight)](https://cran.r-project.org/package=scdv)
+
+## Installation
+
+``` r
+# install.packages("gghighlight")
+
+# The development version from GitHub:
+# install.packages("devtools")
+devtools::install_github("teramonagi/scdv")
+```
+
+## Example
+``` r
+library(scdv)
+```
